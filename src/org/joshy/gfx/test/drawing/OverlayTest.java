3e96b6f5c8f6a177c2af3b6af57af5cdd900ecab
Josh Marinacci <joshua@marinacci.org>
2010-10-28 00:20:21 -0700

    updated overlay test to have an input on node test

--- src/org/joshy/gfx/test/drawing/OverlayTest.java
+++ src/org/joshy/gfx/test/drawing/OverlayTest.java
@@ -3,12 +3,17 @@ package org.joshy.gfx.test.drawing;
 import org.joshy.gfx.Core;
 import org.joshy.gfx.draw.FlatColor;
 import org.joshy.gfx.draw.TransformNode;
+import org.joshy.gfx.event.Callback;
+import org.joshy.gfx.event.EventBus;
+import org.joshy.gfx.event.MouseEvent;
 import org.joshy.gfx.node.Node;
 import org.joshy.gfx.node.control.Button;
 import org.joshy.gfx.node.control.Textbox;
 import org.joshy.gfx.node.layout.Panel;
 import org.joshy.gfx.node.shape.Oval;
+import org.joshy.gfx.node.shape.Shape;
 import org.joshy.gfx.stage.Stage;
+import org.joshy.gfx.util.u;
 
 public class OverlayTest implements Runnable {
 
@@ -20,10 +25,18 @@ public class OverlayTest implements Runnable {
     @Override
     public void run() {
         Node n1 = new TransformNode().setRotate(15).setContent(new Button("Button")).setTranslateX(50);
-        Node n2 = new TransformNode().setRotate(30).setContent(new Oval().setWidth(100).setHeight(30).setFill(FlatColor.GREEN.deriveWithAlpha(0.5))).setTranslateX(80);
+        Shape oval = new Oval().setWidth(100).setHeight(30).setFill(FlatColor.GREEN.deriveWithAlpha(0.5));
+        Node n2 = new TransformNode().setRotate(30).setContent(oval).setTranslateX(80);
         Node n3 = new TransformNode().setRotate(45).setContent(new Textbox("text box")).setTranslateX(100);
         Stage stage = Stage.createStage();
         stage.setContent(new Panel().add(n1,n2,n3));
+
+        EventBus.getSystem().addListener(oval, MouseEvent.MousePressed, new Callback<MouseEvent>(){
+            @Override
+            public void call(MouseEvent event) throws Exception {
+                u.p("pressed");
+            }
+        });
 
     }
 }
