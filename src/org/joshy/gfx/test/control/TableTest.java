feecdee673e18fd7b2d1e6325d02792df4c9da63
Josh Marinacci <joshua@marinacci.org>
2010-12-01 14:27:34 -0800

    move tree table code into it's own proper class

--- src/org/joshy/gfx/test/control/TableTest.java
+++ src/org/joshy/gfx/test/control/TableTest.java
@@ -1,18 +1,12 @@
 package org.joshy.gfx.test.control;
 
 import org.joshy.gfx.Core;
-import org.joshy.gfx.event.EventBus;
-import org.joshy.gfx.event.KeyEvent;
-import org.joshy.gfx.event.MouseEvent;
 import org.joshy.gfx.node.control.Control;
 import org.joshy.gfx.node.control.ScrollPane;
 import org.joshy.gfx.node.control.TableView;
+import org.joshy.gfx.node.control.TreeView;
 import org.joshy.gfx.node.layout.TabPanel;
 import org.joshy.gfx.stage.Stage;
-import org.joshy.gfx.test.control.table.TreeTableDataRenderer;
-import org.joshy.gfx.test.control.table.TreeTableKeyEventHandler;
-import org.joshy.gfx.test.control.table.TreeTableMouseEventHandler;
-import org.joshy.gfx.test.control.table.TreeTableModel;
 
 public class TableTest implements Runnable {
 
@@ -31,13 +25,9 @@ public class TableTest implements Runnable {
     }
 
     private Control tree() {
-        TableView table = new TableView();
-        table.setWidth(500);
-        table.setModel(new TreeTableModel());
-        table.setRenderer(new TreeTableDataRenderer());
-        EventBus.getSystem().addListener(table, MouseEvent.MousePressed, new TreeTableMouseEventHandler());
-        EventBus.getSystem().addListener(table, KeyEvent.KeyPressed, new TreeTableKeyEventHandler());
-        return new ScrollPane(table);
+        TreeView treeView = new TreeView();
+        treeView.setWidth(500);
+        return new ScrollPane(treeView);
     }
 
     private Control standardTable() {
