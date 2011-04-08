da544c26050393fc88d224fba1e863e84df8e497
Josh Marinacci <joshua@marinacci.org>
2011-04-07 20:44:22 -0700

    added autosizing to TextArea added missing getters to the FlatColor class

--- src/org/joshy/gfx/test/control/TextareaTest.java
+++ src/org/joshy/gfx/test/control/TextareaTest.java
@@ -1,9 +1,14 @@
 package org.joshy.gfx.test.control;
 
 import org.joshy.gfx.Core;
+import org.joshy.gfx.draw.FlatColor;
 import org.joshy.gfx.draw.Font;
-import org.joshy.gfx.node.control.ScrollPane;
+import org.joshy.gfx.event.Callback;
+import org.joshy.gfx.event.Event;
+import org.joshy.gfx.event.EventBus;
+import org.joshy.gfx.event.SystemMenuEvent;
 import org.joshy.gfx.node.control.Textarea;
+import org.joshy.gfx.node.layout.Panel;
 import org.joshy.gfx.stage.Stage;
 
 /**
@@ -22,10 +27,21 @@ public class TextareaTest implements Runnable {
 
     @Override
     public void run() {
+        EventBus.getSystem().addListener(SystemMenuEvent.Quit, new Callback<Event>() {
+            @Override
+            public void call(Event event) throws Exception {
+                System.exit(0);
+            }
+        });
         Stage stage = Stage.createStage();
         //stage.setContent(g);
-        final Textarea ta = new Textarea("hello there! hello there! how are you doing today mister man?");
+        final Textarea ta = new Textarea("foo");
+        ta.setSizeToText(true);
         ta.setFont(Font.name("Arial").size(50).resolve());
-        stage.setContent(new ScrollPane(ta));
+        //stage.setContent(new ScrollPane(ta));
+        Panel pa = new Panel();
+        pa.setFill(FlatColor.RED);
+        pa.add(ta);
+        stage.setContent(pa);
     }
 }
