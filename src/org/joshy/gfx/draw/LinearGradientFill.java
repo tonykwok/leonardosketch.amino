38626acd5f363db586ec3a6476f22bfaf1a11799
Josh Marinacci <joshua@marinacci.org>
2011-04-18 10:09:00 -0700

    added snapping to gradient paints added MultiGradientFill.removeStop(stop)

--- src/org/joshy/gfx/draw/LinearGradientFill.java
+++ src/org/joshy/gfx/draw/LinearGradientFill.java
@@ -9,10 +9,16 @@ package org.joshy.gfx.draw;
  */
 public class LinearGradientFill extends MultiGradientFill{
 
+    public static enum Snap {None, Start, Middle, End};
+
     private double startX;
     private double startY;
     private double endX;
     private double endY;
+    private Snap startXSnapped = Snap.None;
+    private Snap startYSnapped = Snap.None;
+    private Snap endXSnapped = Snap.None;
+    private Snap endYSnapped = Snap.None;
 
     public LinearGradientFill setStartX(double startX) {
         this.startX = startX;
@@ -33,6 +39,40 @@ public class LinearGradientFill extends MultiGradientFill{
         this.endY = endY;
         return this;
     }
+
+    public LinearGradientFill setStartXSnapped(Snap snap) {
+        this.startXSnapped = snap;
+        return this;
+    }
+    public LinearGradientFill setStartYSnapped(Snap x) {
+        this.startYSnapped = x;
+        return this;
+    }
+    public LinearGradientFill setEndXSnapped(Snap x) {
+        this.endXSnapped = x;
+        return this;
+    }
+    public LinearGradientFill setEndYSnapped(Snap x) {
+        this.endYSnapped = x;
+        return this;
+    }
+    public Snap getStartXSnapped() {
+        return startXSnapped;
+    }
+
+    public Snap getStartYSnapped() {
+        return startYSnapped;
+    }
+
+    public Snap getEndXSnapped() {
+        return endXSnapped;
+    }
+
+    public Snap getEndYSnapped() {
+        return endYSnapped;
+    }
+
+
 
     @Override
     public Paint duplicate() {
