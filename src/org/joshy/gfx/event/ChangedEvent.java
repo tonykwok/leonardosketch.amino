9f62b8825ee1fa2a093af070bed1af84737879d3
Josh Marinacci <joshua@marinacci.org>
2011-04-26 20:23:16 -0700

    added 'isAdjusting' to changed event

--- src/org/joshy/gfx/event/ChangedEvent.java
+++ src/org/joshy/gfx/event/ChangedEvent.java
@@ -9,11 +9,19 @@ public class ChangedEvent extends Event {
     public static final EventType ObjectChanged = new EventType("ObjectChanged");
     public static final EventType FinalChange = new EventType("FinalChange");
     private Object value;
+    private boolean adjusting = false;
 
     public ChangedEvent(EventType type, Object value, Object source) {
         super(type);
         this.value = value;
         this.source = source;
+    }
+
+    public ChangedEvent(EventType type, Object value, Object source, boolean adjusting) {
+        super(type);
+        this.value = value;
+        this.source = source;
+        this.adjusting = adjusting;
     }
 
     public Object getValue() {
@@ -22,5 +30,9 @@ public class ChangedEvent extends Event {
 
     public boolean getBooleanValue() {
         return (Boolean)value;
+    }
+
+    public boolean isAdjusting() {
+        return this.adjusting;
     }
 }
